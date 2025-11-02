9e42d3145d1a1b7b7d14969152e2117e25565412
Roha Pathan <rohaabropathan@gmail.com>
2025-11-02 13:17:31 +0500

    .

--- architecture.py
+++ architecture.py
@@ -1,6 +1,20 @@
-# tokenizer
-# attention module
-# layer normalization
-# gelu relu whatever that is
-# transformer blocks
-# gpt model
+import torch.nn as nn
+import torch
+import tiktoken
+
+
+GPT_CONFIG_124M = {
+    "vocab_size": 50257, #(because of tiktoken)
+    "context_length": 1024,
+    "emb_dim": 768,
+    "n_heads": 12,
+    "n_layers": 12,
+    "drop_rate": 0.1,
+    "qkv_bias": False
+}
+
+class GPTModel(nn.Module):
+    def __init__(self, cfg):
+        self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
+        self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
+        self.drop_emb = nn.Dropout(cfg["drop_rate"])

67c3a83d3daefb3cd18fd3d82e87201f70b25bb3
row-huh <rohaabropathan@gmail.com>
2026-01-23 11:13:23 +0500

    fixed import errors

--- util.py
+++ util.py
@@ -1,8 +1,9 @@
+import torch
 
 def calc_loss_batch(input_batch, target_batch, model,  device):
     input_batch = input_batch.to(device)
     target_batch = target_batch.to(device)
     logits = model(input_batch)
-    loss = nn.functional.cross_entropy(
+    loss = torch.nn.functional.cross_entropy(
         logits.flatten(0,1), target_batch.flatten()
     )

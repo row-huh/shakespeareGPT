f07c17f1d74e55aa8d60e95bf7278fbfd6c72935
row-huh <rohaabropathan@gmail.com>
2026-01-23 11:11:33 +0500

    implemented loss function to calculate loss per batch

---
+++ util.py
@@ -0,0 +1,8 @@
+
+def calc_loss_batch(input_batch, target_batch, model,  device):
+    input_batch = input_batch.to(device)
+    target_batch = target_batch.to(device)
+    logits = model(input_batch)
+    loss = nn.functional.cross_entropy(
+        logits.flatten(0,1), target_batch.flatten()
+    )
